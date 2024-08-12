85a5cddec677501bb8a25f00e9bc744ecc576b93
Geahad🔻 <167584706+gv3Dev@users.noreply.github.com>
2024-08-11 21:18:02 -0400

    Update adBlock.user.js

--- adBlock.user.js
+++ adBlock.user.js
@@ -1,79 +1,28 @@
 // ==UserScript==
-// @name         No Ads Youtube - Youtube Ad Blocker
+// @name         No ADS - YouTube
 // @namespace    http://tampermonkey.net/
 // @version      3.0
-// @description  Skips all YouTube ads instantly and undetectably, removes annoying elements.
-// @author       GV3Dev
+// @description  - Skips all youtube ads - | - undetectable - | - skips ads instantly -
+// @author       gv3dev
 // @match        https://www.youtube.com/*
 // @match        https://m.youtube.com/*
 // @icon         https://i.ibb.co/X5f50Cg/Screen-Shot-2021-07-19-at-9-31-54-PM.png
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    let originalPlaybackRate = 1;
-
-    const selectAll = (selector) => document.querySelectorAll(selector);
-    const selectFirst = (selector) => document.querySelector(selector);
-
-    const hideElements = (...selectors) => {
-        selectors.flat().forEach(selector => {
-            selectAll(selector).forEach(element => element.style.display = 'none');
-        });
-    };
-
-    const removeElements = (...selectors) => {
-        selectors.flat().forEach(selector => {
-            selectAll(selector).forEach(element => element.remove());
-        });
-    };
-
-    const clickElements = (...selectors) => {
-        selectors.flat().forEach(selector => {
-            selectAll(selector).forEach(element => element.click());
-        });
-    };
-
-    const handleAds = () => {
-        const video = selectFirst('.video-stream.html5-main-video');
-        if (!video) return;
-
-        const adModule = selectFirst('.video-ads.ytp-ad-module');
-        const adText = selectFirst('.ytp-ad-preview-text');
-
-        if (adModule && adText) {
-            video.playbackRate = 16;
-            video.muted = true;
-        } else {
-            video.playbackRate = originalPlaybackRate;
-            video.muted = false;
-        }
-
-        clickElements('.ytp-skip-ad-button', '.ytp-ad-overlay-close-button');
-        hideElements(
-            '.ytp-ad-message-container',
-            '.style-scope.ytd-watch-next-secondary-results-renderer.sparkles-light-cta.GoogleActiveViewElement',
-            '.style-scope.ytd-item-section-renderer.sparkles-light-cta'
-        );
-        removeElements(
-            '#masthead-ad',
-            'ytd-ad-slot-renderer',
-            '.style-scope.ytd-companion-slot-renderer',
-            'ytd-reel-shelf-renderer',
-            '.ytd-popup-container',
-            '.ytp-paid-content-overlay-link'
-        );
-    };
-
-    const initObserver = () => {
-        new MutationObserver(handleAds).observe(document.body, {
-            childList: true,
-            subtree: true,
-        });
-        handleAds();
-    };
-
-    try {
-        initObserver();
-    } catch (error) {}
-})();
+let ogVolume=1,pbRate = 1;setInterval(()=>{
+    if(document.getElementsByClassName("video-stream html5-main-video")[0]!==undefined){let ad = document.getElementsByClassName("video-ads ytp-ad-module")[0];
+        let vid = document.getElementsByClassName("video-stream html5-main-video")[0];if(ad==undefined){pbRate = vid.playbackRate};
+        let closeAble = document.getElementsByClassName("ytp-ad-overlay-close-button");for(let i=0;i<closeAble.length;i++){closeAble[i].click()};
+        if(document.getElementsByClassName("style-scope ytd-watch-next-secondary-results-renderer sparkles-light-cta GoogleActiveViewElement")[0]!==undefined)
+        {let sideAd=document.getElementsByClassName("style-scope ytd-watch-next-secondary-results-renderer sparkles-light-cta GoogleActiveViewElement")[0];sideAd.style.display="none";}
+        if(document.getElementsByClassName("style-scope ytd-item-section-renderer sparkles-light-cta")[0]!==undefined)
+        {let sideAd_ = document.getElementsByClassName("style-scope ytd-item-section-renderer sparkles-light-cta")[0];sideAd_.style.display="none";}
+        if(document.getElementsByClassName("ytp-ad-text ytp-ad-skip-button-text")[0]!==undefined){let skipBtn=document.getElementsByClassName("ytp-ad-text ytp-ad-skip-button-text")[0];skipBtn.click();}
+        if(document.getElementsByClassName("ytp-ad-message-container")[0]!==undefined){let incomingAd=document.getElementsByClassName("ytp-ad-message-container")[0];incomingAd.style.display="none";}
+        if(document.getElementsByClassName("style-scope ytd-companion-slot-renderer")[0]!==undefined){document.getElementsByClassName("style-scope ytd-companion-slot-renderer")[0].remove();}
+        if(ad!==undefined){if(ad.children.length>0){if(document.getElementsByClassName("ytp-ad-text ytp-ad-preview-text")[0]!==undefined)
+        {vid.playbackRate=16;vid.muted=true;}}}if(document.getElementById("masthead-ad")!==null){let headerAd=document.getElementById("masthead-ad");headerAd.remove();}
+        if(document.getElementsByTagName("ytd-ad-slot-renderer")[0]!==undefined){let rightSideAd=document.getElementsByTagName("ytd-ad-slot-renderer")[0];rightSideAd.remove();}
+        if(document.getElementsByTagName("ytd-reel-shelf-renderer")[0]!==undefined){let rightSideShorts=document.getElementsByTagName("ytd-reel-shelf-renderer")[0];rightSideShorts.remove();}}
+},100);
