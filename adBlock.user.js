b11ce75877e0062da6b4b55f531c3363b58c5107
Geahad🔻 <haymorg0473@gmail.com>
2024-08-22 01:57:01 -0400

    Update adBlock.user.js

--- adBlock.user.js
+++ adBlock.user.js
@@ -1,16 +1,38 @@
 // ==UserScript==
 // @name         No Ads - YouTube AdBlocker | Ad Skipper | Free YouTube Music | Ad Remover | Remove Adblock Warning
 // @namespace    http://tampermonkey.net/
-// @version      3.4
+// @version      3.5
 // @description  Skips all YouTube ads instantly and removes banners/overlays. Completely undetectable ad blocker. Enjoy ad-free YouTube music playlists and videos with no interruptions. Removes adblock detection warnings.
 // @author       gv3dev
 // @match        https://*.youtube.com/*
 // @icon         https://i.ibb.co/ZMwwmXm/Screenshot-2024-08-22-at-12-05-51-AM-removebg-preview.png
 // @grant        none
 // @license      MIT
+// @downloadURL  https://update.greasyfork.org/scripts/504197/No%20Ads%20-%20YouTube%20AdBlocker%20%7C%20Ad%20Skipper%20%7C%20Free%20YouTube%20Music%20%7C%20Ad%20Remover%20%7C%20Remove%20Adblock%20Warning.user.js
+// @updateURL    https://update.greasyfork.org/scripts/504197/No%20Ads%20-%20YouTube%20AdBlocker%20%7C%20Ad%20Skipper%20%7C%20Free%20YouTube%20Music%20%7C%20Ad%20Remover%20%7C%20Remove%20Adblock%20Warning.meta.js
 // ==/UserScript==
 
-let ogVolume = 1, pbRate = 1, manualPause = false;
+
+
+let ogVolume = 1, pbRate = 1, manualPause = false; // don't touch, only touch the line below [ if i add more features i'll make a GUI ;) ]
+
+
+const should_Remove_Shorts = true // replace with false then save to allow shorts
+
+
+document.addEventListener('keydown', (event) => {
+    if (event.code === 'Space') {
+        manualPause = true;
+    }
+});
+
+document.addEventListener('play', () => {
+    manualPause = false;
+});
+
+document.addEventListener('pause', () => {
+    manualPause = true;
+});
 
 const manip = (actions) => {
     actions.forEach(({ selector, action, func }) => {
@@ -47,17 +69,23 @@ Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
     }
 });
 
-setInterval(() => {
-    if (location.href == "https://www.youtube.com/" || location.href == "https://m.youtube.com/") {
+
+const removeShorts = () => {
+    if(should_Remove_Shorts == true){
         document.querySelectorAll(".style-scope.ytd-rich-shelf-renderer #title-container").forEach((shelf) => {
-            if (shelf.innerText == "Shorts") {
+            if (shelf.innerText === "Shorts") {
                 shelf.closest('ytd-rich-shelf-renderer').remove();
             }
         });
     }
+};
+
+setInterval(() => {
+
+    removeShorts();
 
     manip([
-        {selector: "tp-yt-paper-dialog", action: 'remove-run', func: playVid()},
+        { selector: "tp-yt-paper-dialog", action: 'remove-run', func: playVid},
         { selector: ".ytp-ad-overlay-close-button", action: 'click' },
         { selector: ".style-scope.ytd-watch-next-secondary-results-renderer.sparkles-light-cta.GoogleActiveViewElement", action: 'hide' },
         { selector: ".style-scope.ytd-item-section-renderer.sparkles-light-cta", action: 'hide' },
@@ -93,17 +121,3 @@ setInterval(() => {
     }
 }, 10);
 
-
-document.addEventListener('keydown', (event) => {
-    if (event.code === 'Space') {
-        manualPause = true;
-    }
-});
-
-document.addEventListener('play', () => {
-    manualPause = false;
-});
-
-document.addEventListener('pause', () => {
-    manualPause = true;
-});
